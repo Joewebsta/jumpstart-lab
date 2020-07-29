8b3b680849b6f71c9a9aba7236ad0f456a5f671d
Joe Webster <joseph.scott.webster@gmail.com>
2020-07-28 20:04:14 -0400

    prompt user for type, message and rotation. Create functionality for when user wants to encrypt

--- ruby/encryptor.rb
+++ ruby/encryptor.rb
@@ -1,4 +1,30 @@
 class Encryptor
+  def run_program
+    collect_info
+  end
+
+  def collect_info
+    puts 'Would you like to encrypt(e) or decrypt(d) a message?'
+    type = gets.chomp.strip.downcase
+    puts 'What is your message?'
+    message = gets.chomp
+    puts 'What is your rotation value?'
+    rotation = gets.chomp.to_i
+
+    run_encryption(message, rotation, type) if type == 'e'
+  end
+
+  def run_encryption(message, rotation, type)
+    encrypted_message = encrypt(message, rotation)
+    display_results(encrypted_message, type)
+  end
+
+  def display_results(message, type)
+    message_type = (type == 'e' ? 'encrypted' : 'decrypted')
+
+    puts "Your #{message_type} message is: #{message}"
+  end
+
   def cipher(rotation)
     chars = (' '..'z').to_a
     # chars = ('a'..'z').to_a + ('A'..'Z').to_a
@@ -53,8 +79,10 @@ class Encryptor
 end
 
 e = Encryptor.new
+e.run_program
+
 # puts e.encrypt('Hello world', 13)
 # puts e.encrypt_file('secret.txt', 13)
 # puts e.decrypt_file('secret.txt.encrypted', 13)
 # e.crack('f w)0/6X0// -6C6` ''46j$( ')
-puts e.crack('\\qmz&%,N&%%q#,9,Vqxx*,`uyq')
+# puts e.crack('\\qmz&%,N&%%q#,9,Vqxx*,`uyq')
